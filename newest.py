2cdf6e0613891db8c01139459ec3f3162da9cff8
alitiger1381 <alitigercr71381@gmail.com>
2024-06-24 20:01:15 +0430

    execute newest part and create json output file

--- newest.py
+++ newest.py
@@ -103,7 +103,7 @@ def extract_article_details(article_link):
         authors = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'authors-accordion-container')))
 
         for i in range(len(authors)):
-            author_name_xpath = '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[1]/div[2]/div[' + str(i+1) + ']/xpl-author-item/div/div[1]/div/div[1]/a/span'
+            author_name_xpath = '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[1]/div[2]/div[' + str(i+1) + ']/xpl-author-item/div/div[1]/div/div[1]/xpl-modal/a/span'
             author_name = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, author_name_xpath))).text
 
             author_from_xpath = '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[1]/div[2]/div[' + str(i+1) + ']/xpl-author-item/div/div[1]/div/div[2]/div'
@@ -118,14 +118,14 @@ def extract_article_details(article_link):
     try:
         keywords_arrow_down_xpath = '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[5]/div[1]/div/i'
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, keywords_arrow_down_xpath))).click()
-        ieee_keywords = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[5]/div[2]/xpl-document-keyword-list/section/div/ul/li[1]/ul')))
+        ieee_keywords = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[4]/div[2]/xpl-document-keyword-list/section/div/ul/li[1]/ul')))
 
         keywords = ieee_keywords.find_elements(By.CLASS_NAME, 'stats-keywords-list-item')
         for keyword in keywords:
             ieee_keywords_list.append(keyword.text)
             print(keyword.text)
 
-        author_keywords = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[5]/div[2]/xpl-document-keyword-list/section/div/ul/li[3]/ul')))
+        author_keywords = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div[3]/div/xpl-root/main/div/xpl-document-details/div/div[1]/div/div[2]/section/div[2]/xpl-accordian-section/div/xpl-document-accordion/div[4]/div[2]/xpl-document-keyword-list/section/div/ul/li[3]/ul')))
         keywords = author_keywords.find_elements(By.CLASS_NAME, 'stats-keywords-list-item')
         for keyword in keywords:
             author_keywords_list.append(keyword.text)
@@ -150,56 +150,6 @@ def extract_article_details(article_link):
         'IEEE Keywords': ieee_keywords_list,
         'Author Keywords': author_keywords_list
     }
-
-
-# def scrape_ieee_xplore(driver):
-#     search_topic = 'Blockchain'
-
-#     driver.get("https://ieeexplore.ieee.org/Xplore/home.jsp")
-
-#     # accept_button_xpath = '/html/body/div[1]/div[2]/div[2]/button[2]'
-#     # WebDriverWait(driver, 25).until(EC.visibility_of_element_located((By.XPATH, accept_button_xpath))).click()
-    
-#     search_bar_xpath = "/html/body/div[5]/div/div/div[3]/div/xpl-root/header/xpl-header/div/div[2]/div[2]/xpl-search-bar-migr/div/form/div[2]/div/div[1]/xpl-typeahead-migr/div/input"
-#     search_bar = WebDriverWait(driver, 25).until(EC.visibility_of_element_located((By.XPATH, search_bar_xpath)))
-#     search_bar.send_keys(search_topic)
-#     search_bar.send_keys(Keys.ENTER)
-
-#     articles_data = []
-#     results_filter = ["Conferences"]
-#     for text in results_filter:
-#         filter_element = WebDriverWait(driver, 20).until(
-#             EC.element_to_be_clickable((By.ID, f'refinement-ContentType:{text}'))
-#         )
-#         filter_element.click()
-#     apply_filter_sel = '.facet-ctype-apply-container > button:nth-child(1)'
-#     apply_filter_button = WebDriverWait(driver, 5).until(
-#         EC.element_to_be_clickable((By.CSS_SELECTOR, apply_filter_sel))
-#     )
-#     apply_filter_button.click()
-
-#     for i in range(1, 6):
-#         print(f"Processing page {i}")
-
-#         article_elements = WebDriverWait(driver, 35).until(
-#             EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.result-item a.fw-bold'))
-#         )
-
-#         for article_element in article_elements:
-#             article_link = article_element.get_attribute('href')
-#             print(article_link)
-#             article_details = extract_article_details(article_link)
-#             if article_details:
-#                 print(article_details)
-#                 articles_data.append(article_details)
-#             time.sleep(5)
-
-#         next_button_sel= '#xplMainContent > div.ng-SearchResults.row.g-0 > div.col > xpl-paginator > div.pagination-bar.hide-mobile.text-base-md-lh > ul > li.next-btn > button'
-#         next_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, next_button_sel)))
-#         next_button.click()
-
-#     with open(f'ieee_articles_{search_topic}.json', 'w') as f:
-#         json.dump(articles_data, f, indent=4)
 
 def scrape_ieee_xplore_newest(driver):
     search_topic = 'Blockchain'
@@ -262,7 +212,7 @@ def scrape_ieee_xplore_newest(driver):
             if article_details:
                 print(article_details)
                 articles_data.append(article_details)
-            time.sleep(5)
+            time.sleep(1)
 
         next_button_sel= '#xplMainContent > div.ng-SearchResults.row.g-0 > div.col > xpl-paginator > div.pagination-bar.hide-mobile.text-base-md-lh > ul > li.next-btn > button'
         next_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, next_button_sel)))
